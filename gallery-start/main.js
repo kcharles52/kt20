06d19a464094041cada59b89236a3587b24f2f3d
charles <kcharles52@gmail.com>
2017-09-19 13:29:08 +0300

    Functionality for the gallary

--- gallery-start/main.js
+++ gallery-start/main.js
@@ -8,8 +8,21 @@ var overlay = document.querySelector('.overlay');
 for(i =1;i<6;i++){
   var newImage = document.createElement('img');
   newImage.setAttribute('src', 'images/pic'+i+'.jpg');
-  thumbBar.appendChild(newImage);	
+  thumbBar.appendChild(newImage);
 }
+
+var images = document.querySelectorAll('.thumb-bar img'); //get all image elements and ref them by images
+
+for(var i =0; i<images.length; i++){ //iterate through the images as you set an onclik mouse event
+	
+	var imgSrc = images[i].getAttribute('src');// get the src attribute value of the current image
+	
+	images[i].onclick=function(e){ //Apply the onclick mouse event to the currently clicked image
+		var imgS = e.target.getAttribute('src'); 
+		displayedImage.setAttribute('src',imgS); // change the attribute of the 
+	}
+}
+
 
 /* Wiring up the Darken/Lighten button */
 
