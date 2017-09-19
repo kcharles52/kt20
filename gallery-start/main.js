60116765b3cde9f859f51621fdf75692bda9b6ff
charles <kcharles52@gmail.com>
2017-09-19 10:49:18 +0300

    gallery

---
+++ gallery-start/main.js
@@ -0,0 +1,15 @@
+var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+for(i =1;i<6;i++){
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic'+i+'.jpg');
+  thumbBar.appendChild(newImage);	
+}
+
+/* Wiring up the Darken/Lighten button */
+
