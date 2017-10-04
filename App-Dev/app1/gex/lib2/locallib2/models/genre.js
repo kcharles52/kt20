5412f6a77cea7f6e8d7a8e6d80589aec3ca0d1a4
kcharles52 <kcharles52@gmail.com>
2017-10-04 15:30:00 +0300

    edited the schema, changed genre description

--- App-Dev/app1/gex/lib2/locallib2/models/genre.js
+++ App-Dev/app1/gex/lib2/locallib2/models/genre.js
@@ -3,15 +3,15 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var genreSchema = new Schema({
-    genre:{type:String,required:true, minlength:3,maxlength:100}
+    name:{type:String,required:true, minlength:3,maxlength:100}
 
 });
 
 //virtual for genre url
 genreSchema
-.virtual('url')
-.get(function(){
-    return '/catalog/genre/'+this._id;
+    .virtual('url')
+    .get(function(){
+        return '/catalog/genre/' + this._id;
 });
 
 //Exporting model
